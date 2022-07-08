ff878eb38538d1de5ca4646d56a08eea9aeb06e9
RaulAdriano <raul.adriano.ramos@gmail.com>
2022-07-08 03:19:55 -0300

    Update

--- CRUD/frontend/src/app/app.module.ts
+++ CRUD/frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort'
 import locatePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(locatePt);
 
@@ -44,7 +45,8 @@ registerLocaleData(locatePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
--- CRUD/frontend/src/app/components/product/product.service.ts
+++ CRUD/frontend/src/app/components/product/product.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { Observable } from 'rxjs';
-import { url } from 'inspector';
+
 
 @Injectable({
   providedIn: 'root'
