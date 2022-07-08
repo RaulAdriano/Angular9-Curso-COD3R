04fb0a5983911a8bd6234cd424a357eac9c5f701
RaulAdriano <raul.adriano.ramos@gmail.com>
2022-07-07 23:44:55 -0300

    aula 21

---
+++ CRUD/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,16 @@
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.scss']
+})
+export class ProductCreateComponent implements OnInit {
+
+  constructor( private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.showOnConsole('Teste....')
+  }
+}
